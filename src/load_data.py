586eb9b6a71d4289477de9d588a4f03843e0e885
tomdoran11 <thomasdoran2@icloud.com>
2026-02-09 13:50:42 +0000

    Edited code to load csv from correct location

--- src/load_data.py
+++ src/load_data.py
@@ -1,9 +1,13 @@
+import os
 import pandas as pd
 
-def load_lap_data(path):
-  data = pd.read_csv(path)
-  return data
+def load_lap_data(filename):
+    # BASE_DIR = folder above src
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    csv_path = os.path.join(BASE_DIR, "data", filename)
+    data = pd.read_csv(csv_path)
+    return data
 
-if __name__ == "__main__":
-  df = load_lap_data("data/raw_lap_data.csv")
-  print(df.head())
+# Call the function correctly with just the CSV file name
+df = load_lap_data("raw_lap_data.csv")
+print(df.head())
