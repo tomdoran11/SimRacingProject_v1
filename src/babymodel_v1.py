e9a500b5ecc60d7f14bf5a373083670068a14118
tomdoran11 <thomasdoran2@icloud.com>
2026-02-11 21:53:45 +0000

    Changed dataset to two decimal places for more detailed predictions. Added and trained S1, S2, S3 prediction models. Removed data due to a bulky print.

--- src/babymodel_v1.py
+++ src/babymodel_v1.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from load_data import load_lap_data
+from sklearn.ensemble import RandomForestRegressor
 
 # Load the data from the csv
 df = load_lap_data("raw_lap_data.csv")
@@ -13,10 +14,51 @@ target = "lap_time"
 X = df[features]
 y = df[target]
 
-# Prints and computes more detailed summary about data
-df.info()
-print(df.isnull().sum())
-df.describe()
+y_s1 = df['sector_1_time']
+y_s2 = df['sector_2_time']
+y_s3 = df['sector_3_time']
+
+# Ensures no data leakage and prevents model from cheating
+assert 'sector_1_time' not in X.columns
+assert 'sector_2_time' not in X.columns
+assert 'sector_3_time' not in X.columns
+
+# Create s1 predicting model
+model_s1 = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
+# Train model
+model_s1.fit(X, y_s1)
+X_text = X.iloc[[0]]
+s1_pred = model_s1.predict(X_text)[0]
+print('S1 Prediction: ', round(s1_pred, 2))
+print('Actual S1 time: ', y_s1.iloc[0])
+
+# Create s2 predicting model
+model_s2 = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
+# Train model
+model_s2.fit(X, y_s2)
+X_text = X.iloc[[0]]
+s2_pred = model_s2.predict(X_text)[0]
+print('S2 Prediction: ', round(s2_pred, 2))
+print('Actual S2 time: ', y_s2.iloc[0])
+
+# Create s3 predicting model
+model_s3 = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
+# Train model
+model_s3.fit(X, y_s3)
+X_text = X.iloc[[0]]
+s3_pred = model_s3.predict(X_text)[0]
+print('S3 Prediction: ', round(s3_pred, 2))
+print('Actual S3 time: ', y_s1.iloc[0])
+
 
 # Split into train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=45)
